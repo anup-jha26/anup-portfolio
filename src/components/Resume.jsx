d6ec7fdd6774c90afb6fd054622dc686aaebdcfa
anup-jha26 <aniljha1076@gmail.com>
2025-07-20 09:23:27 +0530

    Resume update

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -6,7 +6,7 @@ export default function Resume() {
             <h2 className="text-3xl font-bold mb-4">Resume</h2>
             <p className="mb-6">Download my resume PDF below.</p>
             <a
-                href="https://drive.google.com/file/d/1AUU-75KSEetj3Rvc8AhP8QKdGUGOi194"
+                href="https://drive.google.com/file/d/1pIu7fH7DeXxx7ZOwCSpvgWVgCkP5jZ9A"
                 download
                 className="bg-blue-600 text-white px-6 py-3 rounded hover:bg-blue-700"
             >
